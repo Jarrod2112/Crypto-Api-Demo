78b07eb4b0e70435e47e787d65c9f1c03282c35c
Jarrod Cabarubio <jcabarubio@SD-DOM.com>
2021-02-02 16:58:09 -0600

    Updated changes for nre crypto project

--- Axios-Api-Test.js
+++ Axios-Api-Test.js
@@ -1,30 +1,29 @@
 const axios = require("axios").default;
 
 const getData = () => {
-  axios
-    .get("https://api.blockchain.com/v3/exchange/symbols")
-    .then(function (response) {
-      return response.data;
-    })
-    .then(function (data) {
-      const keys = Object.keys(data);
-      console.log(keys);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    axios
+        .get("https://api.blockchain.com/v3/exchange/tickers/LTC-USD")
+        .then(function (response) {
+            return response.data;
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .then(function (tickers) {
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 };
 
-const getData2 = () => {
-  axios
-    .get("https://api.github.com/repos/jreina/blync-party")
-    .then(function (response) {
-      console.log(response.data.issues_url);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-};
 
-getData();
+//getData();
 //getData2();
+
+let test = "test";
+console.log("https://api.blockchain.com/v3/exchange/tickers/" + test);
+
+
+//Create field that excepts a symbol (BTC-USD)
+//Create button the submits GET request for price pertaining to symbol entered.
